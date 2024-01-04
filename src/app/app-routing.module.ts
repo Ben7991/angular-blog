c92171a139d38d71aae4e2c846518b4759a90c8d
Bernard Teye <datssosh@gmail.com>
2024-01-04 20:52:37 +0000

    add admin components to configure all admin pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,51 @@
-import { NgModule } from "@angular/core";
-import { Route, RouterModule, Routes } from "@angular/router";
-import { AdminLayoutComponent } from "./layouts/admin-layout/admin-layout.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { CategoriesComponent } from './pages/admin/categories/categories.component';
+import { TagsComponent } from './pages/admin/tags/tags.component';
+import { PostsComponent } from './pages/admin/posts/posts.component';
+import { CommentsComponent } from './pages/admin/comments/comments.component';
+import { UsersComponent } from './pages/admin/users/users.component';
+import { CreateCategoryComponent } from './pages/admin/categories/create-category/create-category.component';
+import { EditCategoryComponent } from './pages/admin/categories/edit-category/edit-category.component';
+import { CreateTagComponent } from './pages/admin/tags/create-tag/create-tag.component';
+import { EditTagComponent } from './pages/admin/tags/edit-tag/edit-tag.component';
+import { CreatePostComponent } from './pages/admin/posts/create-post/create-post.component';
+import { EditPostComponent } from './pages/admin/posts/edit-post/edit-post.component';
+import { EditCommentComponent } from './pages/admin/comments/edit-comment/edit-comment.component';
+import { CreateUserComponent } from './pages/admin/users/create-user/create-user.component';
+import { EditUserComponent } from './pages/admin/users/edit-user/edit-user.component';
+import { ProfileComponent } from './pages/admin/profile/profile.component';
 
 const appRoute: Routes = [
   {
-    path: "admin", component: AdminLayoutComponent
-  }
+    path: 'admin',
+    component: AdminLayoutComponent,
+    children: [
+      { path: '', component: DashboardComponent, pathMatch: 'full' },
+      { path: 'categories', component: CategoriesComponent },
+      { path: 'categories/create', component: CreateCategoryComponent },
+      { path: 'categories/:id', component: EditCategoryComponent },
+      { path: 'tags', component: TagsComponent },
+      { path: 'tags/create', component: CreateTagComponent },
+      { path: 'tags/:id', component: EditTagComponent },
+      { path: 'posts', component: PostsComponent },
+      { path: 'posts/create', component: CreatePostComponent },
+      { path: 'posts/:id', component: EditPostComponent },
+      { path: 'comments', component: CommentsComponent },
+      { path: 'comments/:id', component: EditCommentComponent },
+      { path: 'users', component: UsersComponent },
+      { path: 'users/create', component: CreateUserComponent },
+      { path: 'users/:id', component: EditUserComponent },
+      { path: 'profile', component: ProfileComponent },
+    ],
+  },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(appRoute)
-  ],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(appRoute)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule {
-
-}
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,54 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CategoriesComponent } from './pages/admin/categories/categories.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { TagsComponent } from './pages/admin/tags/tags.component';
+import { PostsComponent } from './pages/admin/posts/posts.component';
+import { CommentsComponent } from './pages/admin/comments/comments.component';
+import { UsersComponent } from './pages/admin/users/users.component';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { ModelSummaryComponent } from './components/model-summary/model-summary.component';
+import { SummaryComponent } from './components/model-summary/summary/summary.component';
+import { CreateCategoryComponent } from './pages/admin/categories/create-category/create-category.component';
+import { GoBackComponent } from './components/go-back/go-back.component';
+import { EditCategoryComponent } from './pages/admin/categories/edit-category/edit-category.component';
+import { CreateTagComponent } from './pages/admin/tags/create-tag/create-tag.component';
+import { EditTagComponent } from './pages/admin/tags/edit-tag/edit-tag.component';
+import { CreatePostComponent } from './pages/admin/posts/create-post/create-post.component';
+import { EditPostComponent } from './pages/admin/posts/edit-post/edit-post.component';
+import { EditCommentComponent } from './pages/admin/comments/edit-comment/edit-comment.component';
+import { CreateUserComponent } from './pages/admin/users/create-user/create-user.component';
+import { EditUserComponent } from './pages/admin/users/edit-user/edit-user.component';
+import { ProfileComponent } from './pages/admin/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminLayoutComponent
+    AdminLayoutComponent,
+    CategoriesComponent,
+    DashboardComponent,
+    TagsComponent,
+    PostsComponent,
+    CommentsComponent,
+    UsersComponent,
+    PageHeaderComponent,
+    ModelSummaryComponent,
+    SummaryComponent,
+    CreateCategoryComponent,
+    GoBackComponent,
+    EditCategoryComponent,
+    CreateTagComponent,
+    EditTagComponent,
+    CreatePostComponent,
+    EditPostComponent,
+    EditCommentComponent,
+    CreateUserComponent,
+    EditUserComponent,
+    ProfileComponent,
   ],
-  imports: [
-    BrowserModule,
-    NgbModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, NgbModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
