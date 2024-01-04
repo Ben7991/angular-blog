54f35e729638648fe9158894f47b7826882bb491
Bernard Teye <datssosh@gmail.com>
2024-01-04 20:49:17 +0000

    add go back component to traverse to the previous page

---
+++ src/app/components/go-back/go-back.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-go-back',
+  templateUrl: './go-back.component.html',
+  styleUrls: ['./go-back.component.css'],
+})
+export class GoBackComponent {
+  @Input() title = '';
+  @Input() path = '';
+}
