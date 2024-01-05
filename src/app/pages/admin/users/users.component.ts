ecb8410aa4b5c87ff955bf044f8ec42039b044e4
Bernard Teye <datssosh@gmail.com>
2024-01-05 15:24:26 +0000

    add data fetching logic to users component

--- src/app/pages/admin/users/users.component.ts
+++ src/app/pages/admin/users/users.component.ts
@@ -1,19 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { AlertModalComponent } from 'src/app/components/alert-modal/alert-modal.component';
+import { AlertModalDirective } from 'src/app/shared/directives/alert-modal.directive';
+import { UserService } from 'src/app/shared/services/user.service';
 import { ModelSummary } from 'src/app/shared/types/ModelSummary';
+import { CreatedUser } from 'src/app/shared/types/user';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css'],
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  @ViewChild(AlertModalDirective, { static: true })
+  alertModalDirective!: AlertModalDirective;
+  users: CreatedUser[] = [];
   modelSummary: ModelSummary[];
 
-  constructor() {
+  constructor(private userService: UserService, private route: ActivatedRoute) {
     this.modelSummary = [
-      { title: 'All Users', icon: 'people', total: 10 },
-      { title: 'Readers', icon: 'person-vcard', total: 8 },
-      { title: 'Admin', icon: 'person-gear', total: 2 },
+      { title: 'All Users', icon: 'people', total: 0 },
+      { title: 'Readers', icon: 'person-vcard', total: 0 },
+      { title: 'Admin', icon: 'person-gear', total: 0 },
     ];
+  }
+
+  ngOnInit(): void {
+    this.route.data.subscribe({
+      next: (response) => {
+        this.modelSummary[0].total = response['users'].totalUsers;
+        this.modelSummary[1].total = response['users'].totalReaders;
+        this.modelSummary[2].total = response['users'].totalAdmins;
+
+        this.users.push(...response['users'].data);
+      },
+    });
   }
 }
