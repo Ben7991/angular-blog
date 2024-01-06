da7a81e5fd8932dd4d57d4e68baf4409987621ce
Bernard Teye <datssosh@gmail.com>
2024-01-06 10:13:51 +0000

    add LoggedInUser class to user.ts file

--- src/app/shared/types/user.ts
+++ src/app/shared/types/user.ts
@@ -1,3 +1,5 @@
+import { LoggedInUserResponse } from '../services/user.service';
+
 export type User = {
   name: string;
   username: string;
@@ -12,3 +14,35 @@ export type CreatedUser = {
   user_type: string;
   status: string;
 };
+
+export class LoggedInUser {
+  private _id: number;
+  public name: string;
+  private _username: string;
+  public status: string;
+  public user_type: string;
+  private _expirationDate: Date;
+  private _token: string;
+
+  constructor(user: LoggedInUserResponse) {
+    this._id = user.id;
+    this.name = user.name;
+    this._username = user.username;
+    this.status = user.status;
+    this.user_type = user.user_type;
+    this._expirationDate = user.expirationDate;
+    this._token = user.token;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get token() {
+    if (new Date() > this._expirationDate) {
+      return '';
+    }
+
+    return this._token;
+  }
+}
