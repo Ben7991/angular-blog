4cc048862f27727101fffb3d674d8fcc076af186
Bernard Teye <datssosh@gmail.com>
2024-01-07 10:44:03 +0000

    apply logout feature to admin layout component

--- src/app/layouts/admin-layout/admin-layout.component.ts
+++ src/app/layouts/admin-layout/admin-layout.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
   selector: 'app-admin-layout',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AdminLayoutComponent {
   isShown = false;
+
+  constructor(private userService: UserService) {}
+
+  onLogout() {
+    this.userService.logout();
+  }
 }
