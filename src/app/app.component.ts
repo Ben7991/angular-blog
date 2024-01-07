e5ea2cbdb070a51bf711ab2b16fa94d3d0646af3
Bernard Teye <datssosh@gmail.com>
2024-01-07 10:40:26 +0000

    apply autoLogin function to app.component.ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit, Renderer2 } from '@angular/core';
 import { ThemeControllerService } from './shared/services/theme-controller.service';
+import { UserService } from './shared/services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  constructor(private themeController: ThemeControllerService) { }
+  constructor(
+    private themeController: ThemeControllerService,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
     this.themeController.initializeTheme();
+    this.userService.autoLogin();
   }
 }
